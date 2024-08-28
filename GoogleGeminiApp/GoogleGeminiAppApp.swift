1c73f7ffbfa89e9f2c67d0be23e29876942c7703
BigJermaine <113020989+bigjermaine@users.noreply.github.com>
2024-08-28 11:15:43 +0100

    Initial Commit

---
+++ GoogleGeminiApp/GoogleGeminiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleGeminiAppApp.swift
+//  GoogleGeminiApp
+//
+//  Created by MacBook AIR on 28/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct GoogleGeminiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
